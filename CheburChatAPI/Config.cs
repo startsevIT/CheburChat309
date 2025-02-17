2037628313b2dfcac3d8cd1b209e492bb2a8865e
Кирилл <s.t.a.r.t.s.e.v.it@gmail.com>
2025-02-17 15:56:42 +0500

    Clean program file. Incapsule service and app config

---
+++ CheburChatAPI/Config.cs
@@ -0,0 +1,38 @@
+﻿using Infrastructure.Auth;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+public static class Config
+{
+    public static void ServiceConfig(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddAuthorization();
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(o => o.TokenValidationParameters = new TokenValidationParameters
+            {
+                RequireAudience = false,
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+                ValidateIssuerSigningKey = true,
+
+            }
+        );
+        builder.Services.AddCors();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddEndpointsApiExplorer();
+    }
+
+    public static void AppConfig(this WebApplication app)
+    {
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.UseCors(o => o
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowAnyOrigin());
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+}
--- CheburChatAPI/Program.cs
+++ CheburChatAPI/Program.cs
@@ -1,39 +1,12 @@
 using Domain.BusinessEntites.DTOs;
-using Infrastructure.Auth;
 using Infrastructure.DataBase.Repos;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder();
-
-builder.Services.AddAuthorization();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-	.AddJwtBearer(o => o.TokenValidationParameters = new TokenValidationParameters
-	{ 
-		RequireAudience = false,
-		ValidateIssuer = false,
-		ValidateAudience = false,
-        ValidateLifetime = true,
-        IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
-		ValidateIssuerSigningKey = true,
-
-    }
-);
-builder.Services.AddCors();
-builder.Services.AddSwaggerGen();
-builder.Services.AddEndpointsApiExplorer();
+builder.ServiceConfig();
 
 var app = builder.Build();
-
-app.UseAuthentication();
-app.UseAuthorization();
-app.UseCors(o => o
-	.AllowAnyHeader()
-	.AllowAnyMethod()
-	.AllowAnyOrigin());
-app.UseSwagger();
-app.UseSwaggerUI();
+app.AppConfig();
 
 MessageRepo messageRepo = new();
 UserRepo userRepo = new();
@@ -41,15 +14,15 @@ ChatRepo chatRepo = new();
 
 app.MapPost("/users/register", async (RegisterUserDTO dto) =>
 {
-	try
-	{
-		await userRepo.RegisterAsync(dto);
-		return Results.Created();
-	}
-	catch (Exception ex)
-	{
-		return Results.Conflict(ex);
-	}
+    try
+    {
+        await userRepo.RegisterAsync(dto);
+        return Results.Created();
+    }
+    catch (Exception ex)
+    {
+        return Results.Conflict(ex);
+    }
 });
 app.MapPost("/users/login", async (LoginUserDTO dto) =>
 {
@@ -65,14 +38,14 @@ app.MapPost("/users/login", async (LoginUserDTO dto) =>
 app.MapGet("/users/account", [Authorize] async (HttpContext ctx) =>
 {
     Guid id = GetId(ctx);
-	try
-	{
-		return Results.Ok(await userRepo.ReadAsync(id));
-	}
-	catch (Exception ex)
-	{
-		return Results.BadRequest(ex.Message);
-	}
+    try
+    {
+        return Results.Ok(await userRepo.ReadAsync(id));
+    }
+    catch (Exception ex)
+    {
+        return Results.BadRequest(ex.Message);
+    }
 });
 
 app.Run();
