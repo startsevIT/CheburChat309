e42611d892679d66177ec64119620c5ad40c7bce
Кирилл <s.t.a.r.t.s.e.v.it@gmail.com>
2025-02-20 15:58:31 +0500

    Add file with swagger config

---
+++ CheburChatAPI/SwaggerCofig.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+public static class SwaggerCofig
+{
+    public static void AuthCofig(SwaggerGenOptions options)
+    {
+        options.AddSecurityDefinition(
+                name: "Bearer",
+                new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Description = "Вставте в поле токен",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = JwtBearerDefaults.AuthenticationScheme
+                }
+            );
+        options.AddSecurityRequirement(
+        new OpenApiSecurityRequirement
+        {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Name = "Berar",
+                        In = ParameterLocation.Header,
+                        Reference = new OpenApiReference
+                        {
+                            Id = "Bearer",
+                            Type = ReferenceType.SecurityScheme
+                        }
+                    },
+                    new List<string>()
+                }
+        });
+    }
+}
